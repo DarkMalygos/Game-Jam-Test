46ee742fc065061e4eb850dbeac56b2e731aebde
IndianJanos <nakazawa.jatekfejlesztes@gmail.com>
2025-01-05 14:48:19 +0100

    Movement

---
+++ diablo_clone/scripts/entities/main_character/main_character_movement.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+@export var speed = 30.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
