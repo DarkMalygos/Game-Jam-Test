378c34075162ba9b5a1c92274aebcb7faea40c5b
IndianJanos <nakazawa.jatekfejlesztes@gmail.com>
2025-01-07 15:47:26 +0100

    Ability input

--- diablo_clone/scripts/entities/main_character_body/main_character_body.gd
+++ diablo_clone/scripts/entities/main_character_body/main_character_body.gd
@@ -1,8 +1,6 @@
-extends CharacterBody2D
+extends EntityBody
 
-@export var speed = 70
 #@export var jump_velocity = -40.0
-
 
 func _physics_process(delta: float) -> void:
 	#if not is_on_floor():
@@ -11,6 +9,11 @@ func _physics_process(delta: float) -> void:
 	## Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = jump_velocity
+	
+	if Input.is_action_just_pressed("ability_one"):
+		print(1)
+	if Input.is_action_just_pressed("ability_two"):
+		print(2)
 
 	var direction := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
 	if direction:
