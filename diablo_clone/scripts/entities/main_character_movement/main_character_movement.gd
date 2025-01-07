90a206d47cbc8b3370896bd0a031bacc1f65fe29
DarkMalygos <krisztofer.feher@gmail.com>
2025-01-07 13:51:39 +0100

    update

--- diablo_clone/scripts/entities/main_character_movement/main_character_movement.gd
+++ diablo_clone/scripts/entities/main_character_movement/main_character_movement.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 @export var speed = 70
-#@export var jump_velocity = -40.0
+@export var jump_velocity = -40.0
 
 
 func _physics_process(delta: float) -> void:
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = jump_velocity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-	var direction := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		velocity = direction * speed
 	else:
