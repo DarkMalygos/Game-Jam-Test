bfe748d76d454cd4892d45057d926586a3f3f4eb
IndianJanos <nakazawa.jatekfejlesztes@gmail.com>
2025-01-14 12:50:31 +0100

    main character movement updated

--- diablo_clone/scripts/entities/main_character_body/main_character_body.gd
+++ diablo_clone/scripts/entities/main_character_body/main_character_body.gd
@@ -9,6 +9,8 @@ var gravity = -100
 var jump_strength = 80.0
 
 func _physics_process(delta: float) -> void:
+	var direction: Vector2
+	
 	if Input.is_action_just_pressed("jump") and z_position == 0:
 		z_velocity = jump_strength
 	
@@ -18,7 +20,17 @@ func _physics_process(delta: float) -> void:
 	if z_position <= 0:
 		z_position = 0
 		z_velocity = 0
-	
+		
+		direction = Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
+		
+	else:	
+		if Input.is_action_pressed("movement_left"):
+			direction = Vector2(-1, 0)
+		if Input.is_action_pressed("movement_right"):
+			direction = Vector2(1, 0)
+			
+	set_main_character_velocitiy(direction)
+			
 	update_character_position()
 	
 	if Input.is_action_pressed("ability_one"):
@@ -26,20 +38,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ability_two"):
 		weapon.activate(1, self, "enemy", get_global_mouse_position())
 
-	var direction := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
-	if direction:
-		velocity = direction * speed
-	else:
-		velocity = Vector2.ZERO
-
 	move_and_slide()
 
+func set_main_character_velocitiy(direction: Vector2):
+		if direction:
+			velocity = direction * speed
+		else:
+			velocity = Vector2.ZERO
+
 func update_character_position():
-	var screen_position = Vector2(position.x, position.y - z_position)
-	$AnimatedSprite2D.position = screen_position
-	$CollisionShape2D.position = screen_position
-	$Camera2D.position = screen_position
-	weapon.position = screen_position
+	var screen_position = Vector2(global_position.x, global_position.y - z_position)
+	$AnimatedSprite2D.global_position = screen_position
+	$Camera2D.global_position = screen_position
+	weapon.global_position = screen_position
 
 func _reduce_current_health():
 	health_changed.emit()
